7402c431fa4ab9d74e963e861eb7981512fae991
Cédric Reginster <cedric.reginster@gmail.com>
2014-02-27 15:54:37 +0100

    basic structure

---
+++ chrome/background.js
@@ -0,0 +1,11 @@
+/*
+ * A background page: http://developer.chrome.com/extensions/background_pages
+ * 'It exists for the lifetime of your extension, and only one instance of it at a time is active'
+ */
+
+'use strict';
+
+console.log('background.js is ready');
+
+//we can communicate with our content script ;-)
+//http://developer.chrome.com/extensions/messaging
---
+++ chrome/content.js
@@ -0,0 +1,9 @@
+/*
+ * A content script: http://developer.chrome.com/extensions/content_scripts
+ * match: ['http://docs.oracle.com/*', 'https://docs.oracle.com/*']
+ */
+
+console.log('[content.js] Says hello');
+
+//we can communicate with our background script ;-)
+//http://developer.chrome.com/extensions/messaging
