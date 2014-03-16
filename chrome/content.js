b620bd5b28204663270f02eca998af48b9630c01
Cédric Reginster <cedric.reginster@gmail.com>
2014-03-16 18:15:23 +0100

    include jquery, trigger code only on right frame, prepared content wrapper

--- chrome/content.js
+++ chrome/content.js
@@ -3,7 +3,28 @@
  * match: ['http://docs.oracle.com/*', 'https://docs.oracle.com/*']
  */
 
-console.log('[content.js] Says hello');
+'use strict';
+
+var
+  wrapper = '<div class="popular"><ul class="blockList"><li class="blockList"><ul class="blockList"><li id="popular" class="blockList"></li></ul></li></ul></div>',
+  $ = window.jQuery;
+
+/*
+ * we are triggered on all frames, so first check that we are on interesting one
+ * based on highlighted top navigation element ;-)
+ */
+var type = $('.navBarCell1Rev').html(); // [package,Class]
+
+//type class includes Interfaces
+if (type !== 'Class') {
+  //do nothing
+  return true;
+}
+
+//our wrapper
+$('.summary').before($(wrapper));
+
+$('#popular').html('<h3>Popular</h3>');
 
 //we can communicate with our background script ;-)
 //http://developer.chrome.com/extensions/messaging
